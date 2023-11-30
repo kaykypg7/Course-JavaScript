b22fe0f6e62965b3dfc3acdac77512f129b079ed
kaykypg7 <kaykysebastiao35@gmail.com>
2023-11-30 10:50:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const pessoa = {
+        nome: 'Kayky',
+        idade: 25,
+        peso: 65,
+
+        // fala(){                                 //função simples dentro do objeto
+        // console.log('hello word');
+        // }
+        fala(){                                 
+        console.log(`${this.nome} com ${this.idade} anos quer dar um oi...`); // this serve para referenciar o próprio objeto, inves de usar o pessoa.nome , o this substitue a pessoa
+        },
+        
+        adicionarIdade(){ // função simples para adicionar 1 ano de idade quando for chamada
+        this.idade++
+        console.log(this.idade)
+        }
+};
+
+pessoa.fala()
+pessoa.adicionarIdade()
+
