1d677c0522573d5319a9cc4cec4c9da3889b0ad5
kaykypg7 <112106334+kaykypg7@users.noreply.github.com>
2024-03-30 19:05:34 -0300

    setInterval

---
+++ Section 2/setInterval & setTimeout/index.js
@@ -0,0 +1,17 @@
+function horaAtual(){
+    let hora = new Date();
+    return hora.toLocaleTimeString('pt-BR', {
+        
+        hour12 : false
+
+    })
+}
+
+const seconds = setInterval(function(){
+    console.log(horaAtual());
+}, 1000) // milisegundos
+
+setTimeout(function(){clearInterval(seconds)}, 5000) // para o intervalo 
+setTimeout(function(){
+    console.log("Passou-se 10 segundos")
+}, 10000)
